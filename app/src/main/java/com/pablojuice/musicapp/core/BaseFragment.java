85ff88be8a51576d348a53ddbdd2e210439d148b
PabloJuice <pawlo.dzhus@gmail.com>
2021-10-01 20:44:56 +0300

    data and view binding enabled, base fragment created

--- app/src/main/java/com/pablojuice/musicapp/core/BaseFragment.java
+++ app/src/main/java/com/pablojuice/musicapp/core/BaseFragment.java
@@ -1,4 +1,45 @@
 package com.pablojuice.musicapp.core;
 
-public class BaseFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.ViewDataBinding;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+
+public abstract class BaseFragment<B extends ViewDataBinding> extends Fragment {
+
+    protected B binding;
+    protected NavController navController;
+
+    protected abstract B bindLayout(LayoutInflater inflater, ViewGroup container);
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        binding = bindLayout(inflater, container);
+        binding.setLifecycleOwner(getViewLifecycleOwner());
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        try {
+            binding.unbind();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void goBack() {
+        navController.popBackStack();
+    }
 }
