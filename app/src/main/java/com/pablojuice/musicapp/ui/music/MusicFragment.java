c64b9c3948fa839602a35d041c212a9e35a49d4d
PabloJuice <pawlo.dzhus@gmail.com>
2021-10-03 11:37:49 +0300

    player created

--- app/src/main/java/com/pablojuice/musicapp/ui/music/MusicFragment.java
+++ app/src/main/java/com/pablojuice/musicapp/ui/music/MusicFragment.java
@@ -7,13 +7,19 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.pablojuice.musicapp.R;
 import com.pablojuice.musicapp.databinding.FragmentMusicFullScreenBinding;
+import com.pablojuice.musicapp.ui.core.BaseFragment;
 import com.pablojuice.musicapp.ui.core.adapters.MusicFullScreenAdapter;
 import com.pablojuice.musicapp.ui.core.animation.DepthPageTransformer;
-import com.pablojuice.musicapp.ui.core.BaseFragment;
 
 public class MusicFragment extends BaseFragment<FragmentMusicFullScreenBinding> {
+
+    private MusicViewModel viewModel;
 
     @Override
     protected FragmentMusicFullScreenBinding bindLayout(LayoutInflater inflater,
@@ -24,13 +30,20 @@ public class MusicFragment extends BaseFragment<FragmentMusicFullScreenBinding> 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setupViewModel();
+        setupMusicPlayer();
         setupViewPager();
+    }
+
+    private void setupViewModel(){
+        viewModel = new ViewModelProvider(this).get(MusicViewModel.class);
+        viewModel.setItems(MusicFragmentArgs.fromBundle(getArguments()).getMusicItems().getData());
+        viewModel.getIsPlayerReady().observeForever(this::togglePlayBtn);
     }
 
     private void setupViewPager() {
         binding.musicViewPager
-                .setAdapter(new MusicFullScreenAdapter(
-                        MusicFragmentArgs.fromBundle(getArguments()).getMusicItems().getData()));
+                .setAdapter(new MusicFullScreenAdapter(viewModel.getItems()));
         binding.musicViewPager
                 .setCurrentItem(
                         MusicFragmentArgs.fromBundle(getArguments()).getItemPosition(), false);
@@ -38,14 +51,42 @@ public class MusicFragment extends BaseFragment<FragmentMusicFullScreenBinding> 
     }
 
     private void setupMusicPlayer(){
+        viewModel.setupMusicPlayer();
         binding.musicPlayerController.playBtn
-                .setOnClickListener(v -> togglePlayer(binding.musicPlayerController.playBtn.isChecked()));
+                .setOnClickListener(v -> viewModel.togglePlayer());
+        binding.musicViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+
+            @Override
+            public void onPageScrolled(int position,
+                                       float positionOffset,
+                                       int positionOffsetPixels) {
+                togglePlayBtn(false);
+                viewModel.stopPlayer();
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                viewModel
+                        .prepareFromUrl(
+                                viewModel.getItems().get(position).getMusicSrc());
+                super.onPageSelected(position);
+            }
+        });
     }
 
-    private void togglePlayer(boolean status){
-
+    private void togglePlayBtn(boolean isPlayable){
+        binding.musicPlayerController.playBtn.setChecked(false);
+        binding.musicPlayerController.playBtn.setEnabled(isPlayable);
+        binding.musicPlayerController.playBtn
+                .setBackgroundTintList(
+                        ContextCompat.getColorStateList(getContext(),
+                                                        isPlayable ? R.color.green_700:R.color.green_700_transparent));
     }
-    private void changeMusic(){
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        viewModel.releasePlayer();
     }
 }
--- app/src/main/java/com/pablojuice/musicapp/ui/music/MusicViewModel.java
+++ app/src/main/java/com/pablojuice/musicapp/ui/music/MusicViewModel.java
@@ -1,6 +1,78 @@
 package com.pablojuice.musicapp.ui.music;
 
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
+
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.pablojuice.musicapp.model.MusicItem;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
 public class MusicViewModel extends ViewModel {
+
+    private final MutableLiveData<Boolean> isPlayerReady = new MutableLiveData<>(false);
+    private MediaPlayer mediaPlayer;
+    private List<MusicItem> items;
+    private boolean wasPlayed = false;
+
+    public void setupMusicPlayer() {
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioAttributes(
+                new AudioAttributes.Builder()
+                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                        .setUsage(AudioAttributes.USAGE_MEDIA)
+                        .build()
+        );
+    }
+
+    public void prepareFromUrl(String url) {
+        try {
+            mediaPlayer.setDataSource(url);
+            mediaPlayer.setOnPreparedListener(mediaPlayer -> isPlayerReady.postValue(true));
+            mediaPlayer.prepareAsync();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void togglePlayer() {
+        if (mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+        }else {
+            mediaPlayer.start();
+            wasPlayed = true;
+        }
+    }
+    public void stopPlayer(){
+        if (wasPlayed){
+            mediaPlayer.stop();
+        }
+        if (isPlayerReady.getValue()){
+            mediaPlayer.reset();
+        }
+        isPlayerReady.postValue(false);
+    }
+
+    public void releasePlayer() {
+        CompletableFuture.runAsync(() -> {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        });
+    }
+
+    public List<MusicItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<MusicItem> items) {
+        this.items = items;
+    }
+
+    public MutableLiveData<Boolean> getIsPlayerReady() {
+        return isPlayerReady;
+    }
 }
